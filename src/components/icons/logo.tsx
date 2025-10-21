e9a9c1f6574e72b20ca3fa81bdf3bfc8a0319ea0
kiet phan anh <phankiet1233@gmail.com>
2025-10-21 13:20:48 +0000

    đổi tên và logo tốt hơn (_for element div_)

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -2,7 +2,7 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       viewBox="0 0 100 100"
-      aria-label="Manga Text Eraser Logo"
+      aria-label="Clean Slate Logo"
       {...props}
     >
       <defs>
@@ -12,12 +12,10 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
         </linearGradient>
       </defs>
       <path
-        d="M20 80 L20 20 L50 40 L80 20 L80 80 L50 60 Z"
-        stroke="url(#logoGradient)"
-        strokeWidth="10"
-        fill="none"
-        strokeLinejoin="round"
-        strokeLinecap="round"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M50 10C27.9086 10 10 27.9086 10 50C10 72.0914 27.9086 90 50 90V80C33.4315 80 20 66.5685 20 50C20 33.4315 33.4315 20 50 20C66.5685 20 80 33.4315 80 50C80 66.5685 66.5685 80 50 80V90C72.0914 90 90 72.0914 90 50C90 27.9086 72.0914 10 50 10Z"
+        fill="url(#logoGradient)"
       />
     </svg>
   );
